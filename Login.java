ca017af9f6aae379d4af1200b3f37d069c6c3926
jayuk123 <khairnarjayshree29@gmail.com>
2022-11-13 16:19:47 +0530

    message

---
+++ Login.java
@@ -0,0 +1,11 @@
+ public class Login
+ {
+ public static login(String str,int psed) 
+ {
+ System.oout.println("Username is"+str+"Password is "+pswd);
+ }
+ public static void main(String[] args)
+ {
+ login("ABC",123);
+ }
+ }
